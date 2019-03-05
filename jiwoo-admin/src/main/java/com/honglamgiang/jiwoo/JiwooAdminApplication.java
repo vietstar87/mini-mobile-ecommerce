c89546be27ade3049787f33aebcccca75d6eba4a
tranquanghop1987 <tranquanghop1987@gmail.com>
2019-03-05 21:28:01 +0700

    Commit admin

---
+++ jiwoo-admin/src/main/java/com/honglamgiang/jiwoo/JiwooAdminApplication.java
@@ -0,0 +1,13 @@
+package com.honglamgiang.jiwoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiwooAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiwooAdminApplication.class, args);
+	}
+
+}
